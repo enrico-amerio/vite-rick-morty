c68f70fc14b16de0429cd9348a26f5998457c726
enrico-amerio <amerio.enrico95@gmail.com>
2024-03-25 14:54:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+  export default {
+    components:{
+      Header,
+      Main
+    }
+  }
+</script>
+
+<template>
+  
+</template>
+
+
+
+<style lang="scss" scoped>
+@use './assets/scss/main.scss';
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script>
+import {store} from '../data/store'
+  export default {  
+    data(){
+      return{
+        store
+      }
+    }
+    
+  }
+</script>
+
+
+<template>
+ 
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
